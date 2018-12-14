370b6be3cbdaef386ddc28d8bc8989879a94b3ed
Paul Sander <paulleosander@gmail.com>
2018-12-14 13:57:19 -0800

    Add basic callback to Ember.js

--- ember.js
+++ ember.js
@@ -21,6 +21,18 @@
 	},
         
     ready: function() {
+		var eton = window.eton;
+		eton.eton_word_count.calc_money({}, this.wordcountcallback);
+
         console.log(">> Ember RP System ready!");
-    }
+	},
+	
+	// 'this' is the monetary plug-in
+	// 'm' might be posting mode? Full thread, full reply, quick reply?
+	// 'c' is the current amount of points already being awarded
+	// 'l' is 'plugin.settings.word_up.sort(function(a, b) {...}'
+	// return value is the amount of bonus exp to award.
+	wordcountcallback: function(wordcount_data, m, c, l) {
+		return 3;
+	}
 }).init();
